571f38aa292aa1d1658a7c9799c6b92595684414
RomanM1911 <rm73205@srcs.org>
2020-12-17 09:50:50 -0800

    First code problem.

--- scripts/Mazerace.js
+++ scripts/Mazerace.js
@@ -1,6 +1,12 @@
-Mazerace = {
-
+let Mazerace = {
+  Start: undefined,
+  map: document.getElementById("level_map"),
+  circles: [],
+  
     init: function() {
+      for(let i = 0; i < 6; i++) {
+        this.circles.push(this.createCircles());
+      }
     }
   
   }
