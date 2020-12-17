3f565cc3d7fbb761e536f765e7eac0d56d464b58
RomanM1911 <rm73205@srcs.org>
2020-12-17 09:57:26 -0800

    Circles are not showing up the css maybe?

--- scripts/Mazerace.js
+++ scripts/Mazerace.js
@@ -7,6 +7,19 @@ let Mazerace = {
       for(let i = 0; i < 6; i++) {
         this.circles.push(this.createCircles());
       }
+    },
+
+    createCircles: function (x, y) {
+      let circlediv = document.createElement("div")
+      circlediv.className = "circles"
+      let circles = {
+        x_pos: x,
+        y_pos: y,
+        x_velocity: Math.random() * .7,
+        y_velocity: Math.random() * .7,
+        radius: 10,
+        element: circlediv,
+      }
     }
   
   }
