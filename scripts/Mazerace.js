76b653c2422c01cc6c77db367d08ac4c4374993e
Jamieson <jd92327@srcs.org>
2021-01-14 09:51:26 -0800

    update

--- scripts/Mazerace.js
+++ scripts/Mazerace.js
@@ -1,36 +1,50 @@
 let Mazerace = {
   Start: undefined,
   map: document.getElementById("level_map"),
-  circles: [],
-  
-    init: function() {
-      for(let i = 0; i < 6; i++) {
-        this.circles.push(this.createCircles());
-      }
-    },
+  init: function () {
+    for (let i = 1; i < 100; i++) {
+      this.people.push(this.createPlayer1())
+    };
+    this.onkeydown ();
+},
+CreatePlayer1: function() {
+  let player1div = document.createElement("div");
+  player1div.className = "player1";
+  this.Container.append(player1div);
 
-    createCircles: function (x, y) {
-      let circlediv = document.createElement("div")
-      circlediv.className = "circles"
-      let circles = {
-        x_pos: x,
-        y_pos: y,
-        x_velocity: Math.random() * .7,
-        y_velocity: Math.random() * .7,
-        radius: 10,
-        element: circlediv,
+  return player1;
+  },
+  startSimulation: function () {
+    this.simulation = window.setInterval(this.animatePlayer1.bind(Mazerace), 30);
+  },
+
+  animatePlayer1: function () {
+    this.onkeydown ();
+  }
+},
+  onkeydown = function(event) { 
+  console.log(event.keyCode); 
+  if(this.hasBegun == true) {
+   if(event.keyCode == 87) { 
+    //press w 
+    for(let i = 0; i < this.player1.length; i++){ 
+    this.moveUp(this.player1[i]);
+    } 
+    }else if(event.keyCode == 65) { 
+      //press a  
+      for(let i = 0; i < this.player1.length; i++) {
+         this.moveLeft(this.player1[i]); 
+         }
+      } else if(event.keyCode == 68) { 
+        //press d 
+        for(let i = 0; i < this.player1.length; i++) 
+        { this.moveRight(this.player1[i]); } } 
+        else if(event.keyCode == 83) { 
+          //press s 
+          for(let i = e; i < this.player1.length; i++) {
+             this.moveDown(this.player1[i]);
+          }
+        }
       }
     }
-  }
-  var player=document.getElementByID('player1') 
-
-    if (myGameArea.key && myGameArea.key == 37) {myGamePiece.speedX = -1; }
-    if (myGameArea.key && myGameArea.key == 39) {myGamePiece.speedX = 1; }
-    if (myGameArea.key && myGameArea.key == 38) {myGamePiece.speedY = -1; }
-    if (myGameArea.key && myGameArea.key == 40) {myGamePiece.speedY = 1; }
-  }
-
-    
-  
-  
-  Mazerace.init();
+  Mazerace.init()
