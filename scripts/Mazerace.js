8d3e01559365211d5254018dd6371b0e7b465646
RomanM1911 <rm73205@srcs.org>
2021-01-14 09:53:10 -0800

    Need to clear repository so here.

--- scripts/Mazerace.js
+++ scripts/Mazerace.js
@@ -1,27 +1,17 @@
 let Mazerace = {
   Start: undefined,
-  map: document.getElementById("level_map"),
-  circles: [],
+  player1: document.getElementsByClassName("player1"),
+  square: [],
+  
   
     init: function() {
-      for(let i = 0; i < 6; i++) {
-        this.circles.push(this.createCircles());
-      }
-    },
-
-    createCircles: function (x, y) {
-      let circlediv = document.createElement("div")
-      circlediv.className = "circles"
-      let circles = {
-        x_pos: x,
-        y_pos: y,
-        x_velocity: Math.random() * .7,
-        y_velocity: Math.random() * .7,
-        radius: 10,
-        element: circlediv,
+      for(let i = 0; i < this.player1.length; i++) {
+      this.player1[i].keypress = function(event) {
       }
     }
-  
   }
+  movement: function () {
+  }
+} 
   
   Mazerace.init();
